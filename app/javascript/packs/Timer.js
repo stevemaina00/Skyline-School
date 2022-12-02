53f002e5d381b5d042d7852dc43ba29e5387c732
Steve <steve.maina@student.moringaschool.com>
2022-12-02 16:00:47 +0300

    add timer text

--- app/javascript/packs/Timer.js
+++ app/javascript/packs/Timer.js
@@ -8,11 +8,11 @@ function Timer() {
              // By default US English uses 12hr time with AM/PM
 const time = current.toLocaleTimeString("en-US");
   if (currentTime < 12) {
-    greetingText = "Good Morning:The time now is `${currentTime}`"; 
+    greetingText = "Good Morning:The time now is " + time; 
   } else if (currentTime < 18) {
     greetingText = "Good Afternoon:The time now is " + time; 
   } else {
-    greetingText = "Good Evening"; 
+    greetingText = "Good Evening:The time now is " + time; 
   }
   return (
    <div>
