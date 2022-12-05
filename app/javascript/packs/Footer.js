8c8bb966046e6e615c3d8649939b0c2e1dbb4aa6
Steve <steve.maina@student.moringaschool.com>
2022-12-05 14:24:12 +0300

    reduce footer height

--- app/javascript/packs/Footer.js
+++ app/javascript/packs/Footer.js
@@ -8,12 +8,12 @@ function Footer(){
 left:'0',
 bottom:'0',
 right:'0',
-    backgroundColor: "#454545",
+    backgroundColor: "#010203",
     boxSizing: "border-box",
   fontFamily: 'Poppins', 
   width: "100%",
   paddingtop: "0.1px",
-  height: "250px",
+  height: "160px",
   backgroundRepeat: "no-repeat",
   backgroundsize: 'cover',
   backgroundPosition: "center left",
@@ -27,12 +27,12 @@ right:'0',
   return (
     <div>
     <div style={myStyle} className='footer'>
-    <h1 className='h'>Gig-Finder</h1>
+    <h1 className='h'>Skyline School</h1>
     <div>
     <h2 className='i'>Get Started</h2>
     <p>Home</p>
     <p>Register</p>
-    <p>Job Listings</p>
+    <p>Curriculum</p>
     </div>
     <div>
       <h3>About Us</h3>
